ebd4111e754e0b41206a43d238075df8e3ee7ffa
satish-solera <satishsolera54@gmail.com>
2025-12-19 09:17:17 +0530

    fix: fixed the import statments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 import Image from "next/image";
-import { TopNavbarPart } from "./feature/top-navbar-part";
-import { CompanyPart } from "./feature/company-part";
-import { CompanyLogoAnimation } from "./feature/company-logo";
+import { TopNavbarPart } from "../feature/top-navbar-part/index";
+import { CompanyPart } from "../feature/company-part/index";
+import { CompanyLogoAnimation } from "../feature/company-logo/index";
+import { SablePartOne } from "../feature/sable-part-one/index";
+import { SablePartTwo } from "../feature/sable-part-two/index";
+import { SableBrand } from "../feature/sable-brand/index";
+
 
 export default function Home() {
   return (
-    <div className=" min-h-screen  dark:bg-black">
-    <TopNavbarPart/>
-    <CompanyPart/>
-    <CompanyLogoAnimation/>
+    <div className=" min-h-screen ">
+      <TopNavbarPart />
+      <CompanyPart />
+      <CompanyLogoAnimation />
+      <SablePartOne />
+      <SablePartTwo/>
+      
+      <SableBrand/>
+
+
     </div>
   );
 }
