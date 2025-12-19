7edc84091ca4d4b687b7c36cd91853e7539ad98e
satish-solera <satishsolera54@gmail.com>
2025-12-19 09:13:37 +0530

    fix:fixed import route

---
+++ src/component/ui/button.tsx
@@ -0,0 +1,13 @@
+
+import { cn } from '@/util/cn-util'
+import React from 'react'
+
+export const Button = ({ children, className }: { children: React.ReactNode, className?: string }) => {
+  return (
+    <button className={cn('rounded-full p-2 w-fit', className)}>
+      {
+        children
+      }
+    </button>
+  )
+}
