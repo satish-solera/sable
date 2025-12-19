014e242ab0ae9c5fa129d76533b9da130465f830
satish-solera <satishsolera54@gmail.com>
2025-12-19 09:17:45 +0530

    add: added reusable heading

---
+++ src/component/ui/heading.tsx
@@ -0,0 +1,14 @@
+
+import { cn } from '@/util/cn-util'
+import React from 'react'
+
+export const Heading = ({ children , className }: { children: React.ReactNode , className?: string }) => {
+    return (
+
+        <h2 className={cn('text-xs md:text-3xl font-sans ' , className)}>
+            {
+                children
+            }
+        </h2>
+    )
+}
